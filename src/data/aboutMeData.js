c00338e59324bc18b17e5269a506523557988996
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2024-01-07 02:08:04 -0300

    React & TailwindCSS Portfolio

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'Profissional com 3 anos de experiência na codificação,programação, teste e manutenção de sistemas. Com habilidades de comunicação e trabalho em equipe, capaz de promover a integração dos processos e oferecer treinamentos a desenvolvedores e usuários, garantindo o sucesso da implementação dos serviços.',
+	},
+	{
+		id: 2,
+		bio: 'Especializado no desenvolvimento de soluções tecnológicas para clientes corporativos. Com perfil dinâmico e amplo domínio das linguagens de programação, é capaz de otimizar sistemas com foco na resolução de problemas e na melhoria da interface com o usuário.',
+	},
+];
---
+++ src/data/clientsData.js
@@ -0,0 +1,12 @@
+// Import images
+
+
+export const clientsHeading = 'Some of the brands that trust me';
+
+export const clientsData = [
+	{
+		id: 1,
+		title: 'Amazon',
+		img: '#',
+	},
+];
